51d064a61fc08a466581a2038d4f27727e48714a
ComplexAirport <davidbaghdasar@gmail.com>
2025-03-04 17:59:23 +0400

    Revert changes to assembler0

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
 #![allow(unused)]
 #[macro_use]
 
-use my_cpu::my_assembler::assembler0::{*};
-
 
 fn main() {
-    // An example program with our assembler
-    let code: &'static str = ";
-
-
     
-";
-    
-    let src = Source::new("test.txt", code);
-
-    let lexer = Lexer::new(&src);
-    let res = lexer.read_tokens();
-    if res.is_err() {
-        println!("{}", res.unwrap_err())
-    } else {
-        for i in res.unwrap() {
-            println!("{:?}", i);
-        }
-    }
 }
--- src/my_assembler/mod.rs
+++ src/my_assembler/mod.rs
@@ -1,4 +1,3 @@
 pub mod base_assembler;
 pub mod assembler0;
-pub mod text_loc;
 mod error;
